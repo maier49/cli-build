1095551ccc40738e94b9e6b565d34296c86e79ac
Bradley Maier <bmaier@sitepen.com>
2017-07-13 17:27:45 -0400

    Fix path logic, factor out common functionality

--- src/plugins/ExternalDojoLoaderPlugin.ts
+++ src/plugins/ExternalDojoLoaderPlugin.ts
@@ -3,10 +3,8 @@ import * as path from 'path';
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
-/**
- * Describes a dependency on a Dojo 1 module, project, or layer file.
- */
-export type DojoDependency = true | string[] | {
+
+export type DojoDependencyDescriptor = {
 	/**
 	 * The path to the main file to be loaded. Can be provided if this dependency is a folder.
 	 * If not provided, `main.js` will be loaded by default.
@@ -23,7 +21,7 @@ export type DojoDependency = true | string[] | {
 	 * work. A DojoDependency that is an array of strings is equivalent to only defining this property.
 	 *
 	 */
-	packages: string[];
+	packages?: string[];
 
 	/**
 	 * This can be used to specify the location, relative to the externals folder, where the dependency should be copied.
@@ -37,58 +35,53 @@ export type DojoDependency = true | string[] | {
 	fetchImmediately?: boolean;
 };
 
-export interface ExternalDojoDependencyConfig {
-	[ dependency: string ]: DojoDependency;
+/**
+ * Describes a dependency on a Dojo 1 module, project, or layer file.
+ */
+export type DojoDependency = true | string[] | DojoDependencyDescriptor;
+
+export interface ExternalDojoDependencyConfig<T = DojoDependency> {
+	[ dependency: string ]: T;
 }
 
 export default class ExternalDojoLoaderPlugin {
-	private _externals?: ExternalDojoDependencyConfig;
+	private _externals?: ExternalDojoDependencyConfig<DojoDependencyDescriptor>;
 	private _externalConfig?: { [ key: string ]: any };
-	constructor(options: { externals?: ExternalDojoDependencyConfig, externalConfig?: { [ key: string ]: any } }) {
+	constructor(options: { externals?: ExternalDojoDependencyConfig, externalConfig?: { [ key: string ]: any } } = {}) {
 		const { externals, externalConfig } = options;
-		this._externals = externals;
+		if (externals) {
+			this._externals = Object.keys(externals).reduce((newExternals, key) => {
+				const config = externals[key];
+				if (config === true) {
+					newExternals[key] = {};
+				}
+				else if (Array.isArray(config)) {
+					newExternals[key] = { packages: config };
+				}
+				else {
+					newExternals[key] = config;
+				}
+
+				return newExternals;
+			}, {} as ExternalDojoDependencyConfig<DojoDependencyDescriptor>);
+		}
 		this._externalConfig = externalConfig;
 	}
 
 	apply(compiler: Compiler) {
 		const externals = this._externals || {};
-		const externalModules = Object.keys(externals);
-		const loaderModule = externalModules.reduce((prev: string | undefined, next: string) => {
-			if (prev) {
-				return prev;
-			}
+		const loaderModule = this.determineLoaderModule(externals);
 
-			const config = externals[next];
+		compiler.apply(new CopyWebpackPlugin(this.createCopyConfig(externals, loaderModule)));
+		compiler.apply(new HtmlWebpackIncludeAssetsPlugin(this.createHtmlAssetsConfig(externals, loaderModule)));
+	}
 
-			if (config && !Array.isArray(config) && typeof config !== 'boolean' && config.hasLoader) {
-				return next;
-			}
-		}, undefined);
-		const mids = externalModules.filter((module) => module !== loaderModule)
-			.reduce((mids, module) => {
-				const config = externals[module];
-
-				if (config && !Array.isArray(config) && typeof config !== 'boolean' && config.fetchImmediately) {
-					const base = (config.to || module).replace('.js', '');
-					mids.push(config.main ? `'${base}/${config.main}'` : `'${base}'`);
-				}
-
-				return mids;
-			}, [] as string[])
-			.concat([ `'../src/main.js'` ])
-			.join(', ');
-
-		function getDirectoryToCopyTo(module: string) {
-			const config = externals[module];
-			if (config && !Array.isArray(config) && typeof config !== 'boolean' && config.to) {
-				return `externals/${config.to}`;
-			}
-
-			return `externals/${module}`;
-		}
-
-		const copyConfig = [
-			...externalModules.map((module) => ({ from: `node_modules/${module}`, to: getDirectoryToCopyTo(module) })),
+	createCopyConfig(externals: ExternalDojoDependencyConfig<DojoDependencyDescriptor>, loaderModule?: string) {
+		const config = [
+			...Object.keys(externals).map((module) => ({
+				from: `node_modules/${module}`,
+				to: this.getDirectoryToCopyTo(externals, module)
+			})),
 			{
 				from: path.join(__dirname, '../templates/requireExternals.js'),
 				to: 'externals/requireExternals.js',
@@ -97,21 +90,70 @@ export default class ExternalDojoLoaderPlugin {
 						'/* External Config */',
 						this._externalConfig && `${JSON.stringify(this._externalConfig)}, ` || ''
 					)
-					.replace('/* External Layer MIDs */', mids)
+					.replace('/* External Layer MIDs */', this.getMIDs(externals, loaderModule))
 			}
 		];
 
 		if (!loaderModule) {
-			copyConfig.push({ from: path.join(__dirname, '../node_modules/dojo'), to: 'externals/dojo' });
+			config.push({ from: path.join(__dirname, '../node_modules/dojo'), to: 'externals/dojo' });
 		}
 
-		compiler.apply(new CopyWebpackPlugin(copyConfig));
-		compiler.apply(new HtmlWebpackIncludeAssetsPlugin({
+		return config;
+	}
+
+	createHtmlAssetsConfig(externals: ExternalDojoDependencyConfig<DojoDependencyDescriptor>, loaderModule?: string) {
+		const path = loaderModule && this.getPath(loaderModule, externals[loaderModule], true);
+		return {
 			assets: [
-				`externals/${loaderModule ? `${loaderModule}/` : '' }dojo/dojo.js`,
+				`externals/${path || 'dojo/dojo.js'}`,
 				'externals/requireExternals.js'
 			],
-			append: false
-		}));
+		append: false };
+	}
+
+	determineLoaderModule(externals: ExternalDojoDependencyConfig<DojoDependencyDescriptor>) {
+		return Object.keys(externals).reduce((prev: string | undefined, next: string) => {
+			if (prev) {
+				return prev;
+			}
+
+			const config = externals[next];
+
+			if (config && config.hasLoader) {
+				return next;
+			}
+		}, undefined);
+	}
+
+	getDirectoryToCopyTo(externals: ExternalDojoDependencyConfig<DojoDependencyDescriptor>, module: string) {
+		const config = externals[module];
+		if (config && config.to) {
+			return `externals/${config.to}`;
+		}
+
+		return `externals/${module}`;
+	}
+
+	getMIDs(externals: ExternalDojoDependencyConfig<DojoDependencyDescriptor>, loaderModule?: string) {
+		return Object.keys(externals).filter((module) => module !== loaderModule)
+			.reduce((mids, module) => {
+				const config = externals[module];
+
+				if (config && config.fetchImmediately) {
+					mids.push(`'${this.getPath(module, config, false)}'`);
+				}
+
+				return mids;
+			}, [] as string[])
+			.concat([ `'../src/main.js'` ])
+			.join(', ');
+	}
+
+	getPath(module: string, dependency: DojoDependencyDescriptor, includeExtension?: boolean) {
+		const base = dependency.to ? dependency.to : module;
+		const path = dependency.main ? `${base}/${dependency.main}` : base;
+		const hasExtension = /\.js$/.test(path);
+		return includeExtension ?
+			(hasExtension ? path : (path + '.js')) : (hasExtension ? path.replace(/\.js$/, '') : path);
 	}
 }
